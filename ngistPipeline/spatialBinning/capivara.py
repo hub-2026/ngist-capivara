First attempt to fix the SNR computation

ngistPipeline/spatialBinning/capivara.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import os
 
@@ -6,6 +7,7 @@ from scipy import ndimage
 from astropy.io import fits
 from printStatus import printStatus
 from scipy.spatial import cKDTree
+from voronoi import sn_func
 
 """
 PURPOSE:
@@ -29,6 +31,10 @@ def generateSpatialBins(config, cube):
     # Generate the Voronoi bins
     printStatus.running("Defining the segments with Capivara")
     logging.info("Defining the segments with Capivara")
+
+    sn_func_covariances = functools.partial(
+        sn_func, covar_vor=config["SPATIAL_BINNING"]["COVARIANCE"]
+    )
 
     # Read maskfile
     maskfile = (
@@ -69,7 +75,6 @@ def generateSpatialBins(config, cube):
         yNode = ndimage.mean(yv, labels=binMap, index=good)
         
         nPixels = np.bincount(binNum)
-        sn = np.full_like(nPixels, fill_value='99') # To improve
         
         printStatus.updateDone("Defining the segments")
         print("             " + str(np.max(binNum) + 1) + " segments generated!")
@@ -101,7 +106,15 @@ def generateSpatialBins(config, cube):
     binNum_long = np.full(len(cube["x"]), np.nan)
     binNum_long[idxUnmasked] = binNum
     binNum_long[idxMasked] = -1 * binNum_outside
-
+    
+    sn = np.empty_like(nPixels, dtype=float)
+    for i in ubins:
+        idx = np.where(ubins[i] == np.abs(binNum_long))[0]
+        sn[i] = sn_func(idx,
+                        cube["signal"], #[idxUnmasked],
+                        cube["noise"], #[idxUnmasked]
+                        )
+        
     # Save bintable: data for *ALL* spectra inside and outside of the Voronoi region!
     save_table(
         config,
@@ -234,3 +247,7 @@ def save_table(
 
     printStatus.updateDone("Writing: " + config["GENERAL"]["RUN_ID"] + "_table.fits")
     logging.info("Wrote Voronoi table: " + outfits_table)
+
+
+def compute_snr():
+    pass